073dbac9a746abd1afb3378c84fa56fe10ebe105
harryplusplus <harryplusplus@gmail.com>
2026-01-21 14:12:38 +0900

    chore: Change class type inference example

---
+++ packages/examples/src/type-inference/01-class.ts
@@ -0,0 +1,45 @@
+import { type InferDependencies, inject } from 'pocket-di'
+
+// Dependency class
+class Foo {
+  fooFoo() {}
+}
+
+// Infer the type of the dependency declaration object.
+type BarDeps = InferDependencies<typeof Bar>
+/**
+ * type: {
+ *   foo: Foo;
+ * }
+ */
+
+export class Bar {
+  // Define a static inject symbol variable as a {<name>:<class>} declaration
+  // object.
+  static [inject] = { foo: Foo }
+
+  constructor({ foo }: BarDeps) {
+    // Use the dependency object type safely.
+    foo.fooFoo()
+  }
+}
+
+// Infer the type of the dependency declaration object as Context for the
+// instance.
+type BazContext = InferDependencies<typeof Baz>
+
+export class Baz {
+  static [inject] = { foo: Foo }
+
+  c: BazContext
+
+  constructor(c: BazContext) {
+    // Assign to a property without using object destructuring.
+    this.c = c
+  }
+
+  fooFoo() {
+    // Use the dependency object type safely.
+    this.c.foo.fooFoo()
+  }
+}
